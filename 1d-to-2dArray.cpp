758550d6fe29852859e431ca722603cee0d03390
ramya-amin <69972321+ramya-amin@users.noreply.github.com>
2022-09-17 00:22:10 +0530

    Update 1d-to-2dArray.cpp

--- 1d-to-2dArray.cpp
+++ 1d-to-2dArray.cpp
@@ -1,14 +1,6 @@
 /*
 2022. Convert 1D Array Into 2D Array
-Easy
 
-437
-
-31
-
-Add to List
-
-Share
 You are given a 0-indexed 1-dimensional (1D) integer array original, and two integers, m and n. You are tasked with creating a 2-dimensional (2D) array with m rows and n columns using all the elements from original.
 
 The elements from indices 0 to n - 1 (inclusive) of original should form the first row of the constructed 2D array, the elements from indices n to 2 * n - 1 (inclusive) should form the second row of the constructed 2D array, and so on.
@@ -61,5 +53,20 @@ public:
         }
                 
         return mat;
+    }
+};
+
+
+// Optimal solution
+
+class Solution {
+public:
+    vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
+        if(n * m != original.size()) return {};
+        vector<vector<int>> a(m, vector<int>(n));
+        for(int i = 0; i < original.size(); i++) {
+            a[i / n][i % n] = original[i];
+        }
+        return a;
     }
 };
