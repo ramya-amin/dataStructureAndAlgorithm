0daafc14ef398bba61edbb2add4faa4e42dc9fc1
ramya-amin <69972321+ramya-amin@users.noreply.github.com>
2022-11-16 12:24:45 +0530

    palindrome

--- recursion/checkStringPalindrome.cpp
+++ recursion/checkStringPalindrome.cpp
@@ -24,4 +24,47 @@ isPalindrome(start, end) = {     if (start == end) return true;  // odd length p
 
 */
 
+#include <iostream>
+#include <vector>
+#include<cstring>
+#include<cmath>
+
+using namespace std;
+
+class palindrome{
+    
+  public:
+  bool isPalindrome(char* s)
+  {
+    int size=strlen(s)-1;    
+    return checkPalindrome(s, 0, size);  
+        
+  }
+  
+  bool checkPalindrome(char* str, int start, int end) 
+  {
+
+      if(start == end) return true;
+      if(start > end ) return true;
+
+       if(str[start] != str[end]) 
+        return false;
+       else
+        return checkPalindrome(str, start+1, end-1); //check recursively 
+  }
+  
+};
+
+int main()
+{
+    char str[]="aba";
+    palindrome obj;
+
+    if(obj.isPalindrome(str) == 1)
+    cout<<endl<<"Given string is palidrome"<<endl;
+    else
+    cout<<endl<<"Given string is not palindrome"<<endl;
+
+    return 0;
+}
 
